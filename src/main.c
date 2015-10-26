0031008e7a29e36e51994a0b97f9d3802e63f9d8
etienne <owah@outlook.com>
2015-10-26 21:51:35 +0100

    Inputting an array works to produce light

--- src/main.c
+++ src/main.c
@@ -1,10 +1,61 @@
-//
-// Created by etienne on 26/10/15.
-//
 #include <stdio.h>
+#include "../headers/ftd2xx.h"
+
+FT_HANDLE ftHandle;
+FT_STATUS ftStatus;
+ULONG baudRate = 250000;
+
+DWORD bytesWritten;
+char txBuffer[256] = {0xE7, 0xFF, 0x00, 0x00, 0x00, 0x00, 0x00};
+
 
 int main()
 {
-    printf("Hello World!");
+    ftStatus = FT_Open(0, &ftHandle);
+    if (ftStatus == FT_OK)
+        printf("All OK!\n");
+    else
+    {
+        printf("Error! Did you forget to remove ftdi_sio and usbserial?\n");
+        return 1;
+    }
+
+    ftStatus = FT_ResetDevice(ftHandle);
+
+    // Set BaudRate of the device
+    ftStatus = FT_SetBaudRate(ftHandle, baudRate);
+    if (ftStatus == FT_OK)
+        printf("BaudRate successfully set to %d.\n", baudRate);
+    else
+    {
+        printf("BaudRate could not be set. Exiting...\n");
+        return 1;
+    }
+
+    ftStatus = FT_SetDataCharacteristics(ftHandle, FT_BITS_8, FT_STOP_BITS_2, FT_PARITY_NONE);
+    if (ftStatus == FT_OK)
+        printf("Bits per word: 8. Number of stop bits: 2. No parity.");
+    else
+    {
+        printf("Couldn't set data characteristics. Exiting...\n");
+        return 1;
+    }
+
+    ftStatus = FT_SetFlowControl(ftHandle, 0, 0, 0);
+
+    do {
+        // Write data to the device
+        ftStatus = FT_Write(ftHandle, txBuffer, sizeof(txBuffer), &bytesWritten);
+        if (ftStatus != FT_OK)
+        {
+            printf("Error while writing to device. Exiting...\n");
+            return 1;
+        }
+    } while (1);
+
+
+
+
+    FT_Close(ftHandle);
     return 0;
 }
